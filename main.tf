4cecd0e4a48c9765ccd95cd8c0ed673223e50da3
bala <balu391balu@gmail.com>
2022-04-08 09:19:06 +0530

    first

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+resource "aws_instance" "instance" {
+    count = 1
+    ami = "ami-0ab0bbbd329f565e6"
+    instance_type = "t2.micro"
+    key_name = "local"
+    vpc_security_group_ids = ["terra-sg"]
+    
+
+    tags = {
+        Name = "t1"
+    }
+}
+
